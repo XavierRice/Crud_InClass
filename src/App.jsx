739538760e37cc1de8cc705d0d8eb472b83a3646
xavierrice <xavier.rice@gmail.com>
2023-08-11 12:04:17 -0400

    first commit. boiler plate removed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>XXX</h1>
+    
+    </>
+  )
+}
+
+export default App
