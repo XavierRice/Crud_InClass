28940b3fb5b4d2dc232799195601547b9bc1c69c
xavierrice <xavier.rice@gmail.com>
2023-08-11 12:27:52 -0400

    added env

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+const secret = import.meta.env.VITE_10_VAR
 
 import './App.css'
 
 function App() {
 
+
+
   return (
     <>
       <h1>XXX</h1>
-    
+      <h3>TEST</h3>
     </>
   )
 }
