a2eb643fe1d5ec2e2bcdf361b7856c99c462c9a3
xavierrice <xavier.rice@gmail.com>
2023-08-14 19:35:33 -0400

    added a select and tried to make a PATCH

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -8,7 +8,8 @@ const Cards = ({ product, allShows, setAllShows }) => {
 
     const [formObj, setFormObj] = useState({ title: "", country: "", dateAdded: "", releaseYear: null, rating: "", duration: "", listedIn: "", description: "" })
 
-    
+    const [select, setSelect] = useState("")
+    const [change, setChange] = useState("")
 
     function handleDelete(showToDelete) {
 
@@ -27,9 +28,21 @@ const Cards = ({ product, allShows, setAllShows }) => {
 
     }
 
+    function handleSelect(e){
+     setSelect(e.target.value)
+    }
+
     function handleEdit() {
+    
+        const changedObj = {
+     [select]:change
+        };
 
+        const updateShow = {...product, ...changedObj}
 
+        fetch(`http://localhost:5001/api/shows/${product.id}`, { method: "PATCH", "Content-Type": "application/json", body:JSON.stringify(updateShow) })
+        .then(r => r.json())
+        .then(console.log)
 
     }
 
@@ -38,15 +51,20 @@ const Cards = ({ product, allShows, setAllShows }) => {
         <div key={product.id}>
             <h4>{product.title}</h4>
             <form className="edit-form">
-                <label htmlFor="entry1">
+                <label htmlFor="entry1">Choose a Key:
+                    <select name="entry1" id="entry1">
+                      <option value="title" onChange={handleSelect}>Title</option>
+                      <option value="country">Country</option>
+                      <option value="dateAdded">Date Added</option>
+                      <option value="releaseYear">Year Released?</option>
+                      <option value="rating">rating</option>
+                      <option value="duration">duration</option>
+                      <option value="listdIn">Listed-In</option>
+                      <option value="description">Description</option>
+                      </select>
                     <input
                         type="text"
-                        onChange={(se) => se.target.value}
-                        id="key"
-                    />
-                    <input
-                        type="text"
-                        onChange={(se) => se.target.value}
+                        onChange={(se) => setChange(se.target.value)}
                         id="value"
                     />  
                     <button onClick={(se) => handleEdit(product)}>⭐️</button>
